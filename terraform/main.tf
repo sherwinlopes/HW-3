f8bf4da21bbceffd95cc33936762ac3ed218ee3d
Sherwin Lopes <sherwinlop05@gmail.com>
2025-02-26 16:11:20 -0600

    final-deploy

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,43 +4,31 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "myResourceGroup"
-  location = "centralus"
+  name     = "react-app-rg"
+  location = "Central US"
 }
 
-# Static Web App
-resource "azurerm_static_web_app" "static_app" {
-  name                = "myStaticWebApp"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  sku_tier            = "Free"
-}
-
-# App Service Plan
-resource "azurerm_service_plan" "app_service_plan" {
-  name                = "myAppServicePlan"
+# Updated to use azurerm_service_plan
+resource "azurerm_service_plan" "app_plan" {
+  name                = "react-app-plan"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  sku_name            = "B1"  # Basic Tier, adjust as needed
   os_type             = "Linux"
+  sku_name            = "F1"
 }
 
-# Azure Web App
-resource "azurerm_linux_web_app" "web_app" {
-  name                = "myUniqueWebApp1234"
+# Web App
+resource "azurerm_app_service" "app" {
+  name                = "react-color-app-12345"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  service_plan_id     = azurerm_service_plan.app_service_plan.id
+  app_service_plan_id = azurerm_service_plan.app_plan.id
 
   site_config {
-    always_on = true
+    linux_fx_version = "NODE|18-lts"
   }
 }
 
-output "static_webapp_url" {
-  value = azurerm_static_web_app.static_app.default_host_name
-}
-
-output "webapp_url" {
-  value = azurerm_linux_web_app.web_app.default_hostname
+output "app_url" {
+  value = "https://${azurerm_app_service.app.default_site_hostname}"
 }
